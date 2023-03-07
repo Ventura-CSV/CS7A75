90b3eac8d49c40f79622f82b78203409767e799c
Kyuwoong Lee <kwooler@gmail.com>
2023-03-06 22:28:22 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    main.numbers = [5, 20, 30, 30, 35]
+    delval = int(input('Enter the deletion value: '))
+    ##################################################
+    # Code your program here
+    ##################################################
+
+##
+
+
+if __name__ == '__main__':
+    main()
---
+++ main_test.py
@@ -0,0 +1,76 @@
+import main
+import io
+import sys
+import re
+import math
+
+
+def test_main_1():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '30\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    n = len(main.main.numbers)
+    assert n == 3
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '35\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    n = len(main.main.numbers)
+    assert n == 4
+
+
+def test_main_3():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = '75\n'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*1'
+    # regex_string += r'[\w,\W]*3'
+    # regex_string += r'[\w,\W]*5'
+    # regex_string += r'[\w,\W]*'
+    # print(regex_string)
+    # res = re.search(regex_string, main.evenlist)
+    # assert res != None
+    # print(res.group())
+    assert not main.main.numbers == True
